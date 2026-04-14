bc32676ccaf5b89b659aa851eb9e47d2fae5be5f
Azol06 <karinaoliveira25050@gmail.com>
2026-04-14 15:47:07 -0300

    My Vessel program

---
+++ MyVessel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyVessel
+{
+    class Program
+    {
+        static void Main()
+        {
+            VesselEngine TinyVessel = new VesselEngine();
+
+            TinyVessel.Start();
+            TinyVessel.TurnOff();
+        }
+    }
+}
