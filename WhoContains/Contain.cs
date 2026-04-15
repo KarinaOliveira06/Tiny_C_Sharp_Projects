7eb29926d12400509284a03c9caa663b4f44994a
Azol06 <karinaoliveira25050@gmail.com>
2026-04-15 13:16:02 -0300

    Add structure to make WhoContains game

---
+++ WhoContains/Contain.cs
@@ -0,0 +1,8 @@
+char letter = "a";
+char number = "1";
+char character = "@";
+char space = " ";
+
+//Apenas a estrutura para fazer um minigame baseado na função contain, onde o usuário tem que escrever qual das palavras tem a letra correta dentro de um tempo limite.
+//Just the structure to make a game based on "Contain" function, where the user needs to write the correct word within the time limit.
+//Coming soon
