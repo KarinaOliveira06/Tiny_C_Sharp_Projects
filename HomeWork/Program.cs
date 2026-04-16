41d49b567e92f4d3df9255c105db797d60dd91a5
Azol06 <karinaoliveira25050@gmail.com>
2026-04-16 17:19:01 -0300

    String Interpolation

---
+++ HomeWork/Program.cs
@@ -0,0 +1,19 @@
+//text operations
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            string idade = Console.ReadLine();
+
+            string saudacoes = $"Olá {nome}, sua idade é de {idade} anos";
+            Console.WriteLine(saudacoes);
+        }
+    }
+}
