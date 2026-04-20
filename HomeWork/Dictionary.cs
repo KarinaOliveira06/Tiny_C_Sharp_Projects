ad22a7f3c1c97064ec4f0badd9ea428999bbf929
Azol06 <karinaoliveira25050@gmail.com>
2026-04-20 14:03:38 -0300

    Dictionary lesson

---
+++ HomeWork/Dictionary.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+
+namespace HomeWork
+{
+    class Disctionary
+    {
+        static void Main()
+        {
+            Dictionary<int, string> MyDictionary = new Dictionary<int, string>();
+
+            MyDictionary.Add(1, "Hotdog");
+            MyDictionary.Add(2, "Hamburguer");
+
+            Console.WriteLine("Choose the number 1 or the number 2");
+            String Choose = Console.ReadLine();
+
+            bool strChoose = int.TryParse(Choose, out int Result);
+
+            Console.WriteLine($"You've got {MyDictionary[Result]}!");
+
+        }
+    }
+}
